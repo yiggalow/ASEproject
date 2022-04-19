fc9b32a40d7a2bc0a1cca163b6ed7bdd21325323
Danny <danny.kroll@gmx.de>
2022-04-19 14:28:23 +0200

    Created dtos

---
+++ Software Project/1 - Backend/1-Adapters/src/main/java/de/dhbw/ase/mangacollector/episode/EpisodeDTO.java
@@ -0,0 +1,72 @@
+package de.dhbw.ase.mangacollector.episode;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import de.dhbw.ase.mangacollector.manga.MangaDTO;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "id",
+        "title",
+        "manga"
+})
+@Generated("jsonschema2pojo")
+public class EpisodeDTO {
+
+    @JsonProperty("id")
+    private Integer id;
+    @JsonProperty("title")
+    private String title;
+    @JsonProperty("manga")
+    private MangaDTO manga;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("title")
+    public String getTitle() {
+        return title;
+    }
+
+    @JsonProperty("title")
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @JsonProperty("manga")
+    public MangaDTO getManga() {
+        return manga;
+    }
+
+    @JsonProperty("manga")
+    public void setManga(MangaDTO manga) {
+        this.manga = manga;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
